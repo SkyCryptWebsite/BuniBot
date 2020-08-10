0c319ada4d31e70ab8104f879223445f06fa8e67
Shiiyu <faithiikittii@gmail.com>
2020-08-10 01:37:35 -0400

    added commandHelp function

--- discord.js
+++ discord.js
@@ -1,17 +1,11 @@
 const config = require('./config.json');
+
 const levenshtein = require('js-levenshtein');
 
-exports.resolveCommand = (msg, command, commands) => {
-  let keys = Array.from(commands.keys());
-  if (keys.includes(command)) return command;
-  if (!config.owners.includes(msg.author.id)) {
-    keys = keys.filter(key => !config.ownerCommands.includes(key));
-  }
-  const key = keys.sort((a, b) => {
-    return levenshtein(command, a) - levenshtein(command, b);
-  })[0];
-  if (levenshtein(command, key) > 3) return false;
-  return key;
+exports.commandHelp = (msg, client, command) => {
+  const cmd = client.commands.get(command);
+  let fields = [{name: "Command", value: `\`${config.prefix+command}\``}, {name: "Description", value: cmd.description}, {name: "Usage", value: `\`\`\`${config.prefix+command+" "+cmd.usage}\`\`\``}];
+  msg.channel.createMessage(exports.embed(msg, undefined, undefined, fields));
 }
 
 exports.embed = (msg, text, header, fields) => {
@@ -25,8 +19,20 @@ exports.embed = (msg, text, header, fields) => {
         icon_url: msg.author.avatarURL,
         text: `Requested by ${msg.author.username}#${msg.author.discriminator}`
       },
-      timestamp: new Date(),
       color: 0xBAED91
     }
   }
+}
+
+exports.resolveCommand = (msg, command, commands) => {
+  let keys = Array.from(commands.keys());
+  if (keys.includes(command)) return command;
+  if (!config.owners.includes(msg.author.id)) {
+    keys = keys.filter(key => !config.ownerCommands.includes(key));
+  }
+  const key = keys.sort((a, b) => {
+    return levenshtein(command, a) - levenshtein(command, b);
+  })[0];
+  if (levenshtein(command, key) > 3) return false;
+  return key;
 }
