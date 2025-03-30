e33310ef3c4ff16dcdc5bf0bcf10d57767afa3a7
Shiiyu <40838999+Shiiyu@users.noreply.github.com>
2025-03-30 16:48:35 -0400

    remove baazar products

--- functions.js
+++ functions.js
@@ -1,37 +1,6 @@
 const distance = require("jaro-winkler");
 
 module.exports = {
-  getBazaarProduct: (query, products) => {
-    let resultMatch;
-    let itemResults = [];
-    for (const key in products) {
-      itemResults.push({...products[key]});
-    }
-    for (const product of itemResults) {
-      if (product.name.toLowerCase() == query) {
-        resultMatch = product;
-      }
-      product.tagMatches = 0;
-      product.distance = distance(product.name, query, {caseSensitive: false});
-      for (const part of query.split(" ")) {
-        for (const tag of product.tag) {
-          if (tag == part) {
-            product.tagMatches++;
-          }
-        }
-      }
-    }
-    itemResults = itemResults.sort((a, b) => {
-      if (a.tagMatches > b.tagMatches) return -1;
-      if (a.tagMatches < b.tagMatches) return 1;
-      if (a.distance > b.distance) return -1;
-      if (a.distance < b.distance) return 1;
-    });
-    if (!resultMatch) {
-      resultMatch = itemResults[0];
-    }
-    return resultMatch;
-  },
   formatNumber: (number, floor, rounding = 10) => {
     let roundFunc = floor ? Math.floor : Math.ceil;
     if (number < 1000) {
